2792a3bdd456f4d2b89cbb0fc71a115ae957858c
Charly Riviere <crw.riviere@gmail.com>
2017-09-25 23:08:20 +0100

    Updated rules

--- src/models/rules.js
+++ src/models/rules.js
@@ -1,58 +1,57 @@
-const rules = new Map(
-    [
-        ['A', {
-            title: 'Waterfall',
-            description: 'Everyone must keep drinking until the person who picked the card stops.'
-        }],
-        ['2', {
-            title: 'Choose',
-            description: 'Choose someone to drink.'
-        }],
-        ['3', {
-            title: 'Me',
-            description: 'You must drink.'
-        }],
-        ['4', {
-            title: 'Floor',
-            description: 'Last one to touch the floor must drink.'
-        }],
-        ['5', {
-            title: 'Thumb Master',
-            description: 'When you put your thumb on the table everyone must follow, whomever is last must drink.'
-        }],
-        ['6', {
-            title: 'Dunno lol',
-            description: 'Dunno lol'
-        }],
-        ['7', {
-            title: 'Heaven',
-            description: 'Last one to point to the sky must drink'
-        }],
-        ['8', {
-            title: 'Mate',
-            description: 'Choose someone to drink with you'
-        }],
-        ['9', {
-            title: 'Rhyme',
-            description: 'Choose a sentence and the next person must match that sentence with a rhyme, move on to the next person until someone fails to make a rhyme. Whoever fails must drink.'
-        }],
-        ['10', {
-            title: 'Categories',
-            description: 'Pick a category and the next person must match a word in that topic, move on to the next person until someone fails to match a category. Whoever fails must drink.'
-        }],
-        ['J', {
-            title: 'Make a Rule',
-            description: 'Make up a rule that everyone has to obey, anyone who fails to follow the rule must drink.'
-        }],
-        ['Q', {
-            title: 'Question Master',
-            description: 'Anyone who answers your question must drink, this rule is only valid until someone else becomes question master.'
-        }],
-        ['K', {
-            title: 'King',
-            description: 'You must pour your drink into the communal pint, last person to pick the King card must drink the communal pint.'
-        }],
-    ]
-)
+export default new Map(
+  [
+    ['A', {
+      title: 'Waterfall',
+      description: 'Everyone must keep drinking until the person who picked the card stops.',
+    }],
+    ['2', {
+      title: 'Choose',
+      description: 'Choose someone to drink.',
+    }],
+    ['3', {
+      title: 'Me',
+      description: 'You must drink.',
+    }],
+    ['4', {
+      title: 'Floor',
+      description: 'Last one to touch the floor must drink.',
+    }],
+    ['5', {
+      title: 'Thumb Master',
+      description: 'When you put your thumb on the table everyone must follow, whomever is last must drink.',
+    }],
+    ['6', {
+      title: 'Mate',
+      description: 'Choose someone to drink with you',
+    }],
+    ['7', {
+      title: 'Heaven',
+      description: 'Last one to point to the sky must drink',
+    }],
+    ['8', {
+      title: 'Straight',
+      description: 'Whoever is opposite you must drink',
+    }],
+    ['9', {
+      title: 'Rhyme',
+      description: 'Choose a sentence and the next person must match that sentence with a rhyme, move on to the next person until someone fails to make a rhyme. Whoever fails must drink.',
+    }],
+    ['10', {
+      title: 'Categories',
+      description: 'Pick a category and the next person must match a word in that topic, move on to the next person until someone fails to match a category. Whoever fails must drink.',
+    }],
+    ['J', {
+      title: 'Make a Rule',
+      description: 'Make up a rule that everyone has to obey, anyone who fails to follow the rule must drink.',
+    }],
+    ['Q', {
+      title: 'Question Master',
+      description: 'Anyone who answers your question must drink, this rule is only valid until someone else becomes question master.',
+    }],
+    ['K', {
+      title: 'King',
+      description: 'You must pour your drink into the communal pint, last person to pick the King card must drink the communal pint.',
+    }],
+  ],
+);
 
-export {rules}
